6680664b724389750b6546e9807ecaf699335c50
rillyv <rillyv.dev@gmail.com>
2025-10-05 17:11:20 +0200

    Add rate limiting on login and write endpoints (5 requests per minute)

--- api/routes/api.php
+++ api/routes/api.php
@@ -9,12 +9,12 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/auth/login', [AuthController::class, 'login']);
+Route::middleware('throttle:5,1')->post('/auth/login', [AuthController::class, 'login']);
 
 Route::get('/articles', [ArticleController::class, 'index']);
 Route::get('/articles/{article}', [ArticleController::class, 'show']);
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware(['throttle:5,1', 'auth:sanctum'])->group(function () {
     Route::post('/articles', [ArticleController::class, 'store']);
     Route::put('/articles/{id}', [ArticleController::class, 'update']);
     Route::delete('/articles/{id}', [ArticleController::class, 'destroy']);
